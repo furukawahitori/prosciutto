840330e482b31040df957bb8d2516828142a3be8
「ひとり」深見 <113910432+furukawahitori@users.noreply.github.com>
2026-01-31 14:21:22 +0800

    Update load.js

--- load.js
+++ load.js
@@ -1,1 +1,8 @@
-$gameMessage.add("load");
+//a modification for Koukou Seikatsu
+$gameMessage.add("init successfully loaded from\nhttps://raw.githubusercontent.com/furukawahitori/prosciutto/refs/heads/main/load.js");
+$gameParty.addActor(2);
+$gameParty.addActor(3);
+$gameParty.addActor(4);
+$gameParty.gainItem($dataItems[10], 1);
+
+AudioManager.playBgm({name: "hitominikki_milkymilkyocean", volume: 100, pitch: 100, pan: 0});
