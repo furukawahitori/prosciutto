7552e79e22aa71c4e2078d718105ff0dae8ba55e
「ひとり」深見 <113910432+furukawahitori@users.noreply.github.com>
2026-01-31 14:26:45 +0800

    Update load.js

--- load.js
+++ load.js
@@ -3,6 +3,7 @@ $gameMessage.add("init successfully loaded from\nMenherafureshia's Prosciutto Mo
 $gameParty.addActor(2);
 $gameParty.addActor(3);
 $gameParty.addActor(4);
+$gameMessage.add("\P[1] has received hitomi nikki (x1)");
 $gameParty.gainItem($dataItems[10], 1);
 
 AudioManager.playBgm({name: "hitominikki_milkymilkyocean", volume: 100, pitch: 100, pan: 0});
